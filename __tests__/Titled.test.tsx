1c68c45d519e1caa22c3df41891323410a291c7f
Slava Knyazev <slava@knyazev.io>
2022-11-12 00:38:35 +0000

    Allow string for setting title without function

--- __tests__/Titled.test.tsx
+++ __tests__/Titled.test.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import { expect, test, vi } from 'vitest';
+import { beforeEach, expect, test, vi } from 'vitest';
 import { Titled } from '../src/Titled';
+
+beforeEach(() => {
+  document.title = '';
+});
 
 test('should output single title', () => {
   const handleChange = vi.fn();
@@ -12,6 +16,16 @@ test('should output single title', () => {
   );
   expect(handleChange).toHaveBeenCalledTimes(1);
   expect(handleChange.mock.calls[0][0]).toBe('Foo');
+});
+
+test('should override change handler', () => {
+  let titleToSet = '';
+  render(
+    <Titled title={() => 'Foo'} onChange={title => (titleToSet = title)}>
+      Child
+    </Titled>
+  );
+  expect(titleToSet).toBe('Foo');
 });
 
 test('should output nested title', () => {
@@ -40,4 +54,24 @@ test('should renrender nested title', () => {
   render(<Titled title={() => 'A'} onChange={handleChange} />);
   expect(handleChange).toHaveBeenCalledTimes(2);
   expect(handleChange.mock.calls[1][0]).toBe('A');
+});
+
+test('should handle string literal title', () => {
+  const handleChange = vi.fn();
+  render(<Titled title={'A'} onChange={handleChange}></Titled>);
+  expect(handleChange).toHaveBeenCalledTimes(1);
+  expect(handleChange.mock.calls[0][0]).toBe('A');
+});
+
+test('should handle nested string literal title', () => {
+  const handleChange = vi.fn();
+  render(
+    <Titled title={() => 'A'} onChange={handleChange}>
+      <Titled title={'B'}>
+        <Titled title={title => `C | ${title}`}>Child</Titled>
+      </Titled>
+    </Titled>
+  );
+  expect(handleChange).toHaveBeenCalledTimes(1);
+  expect(handleChange.mock.calls[0][0]).toBe('C | B');
 });
