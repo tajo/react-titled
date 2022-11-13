3cbcd987f7204b18b38599ba70c232986e9d2e18
Slava Knyazev <slava@knyazev.io>
2022-11-13 19:59:21 +0000

    Bump deps

--- examples/index.tsx
+++ examples/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Titled } from '../src/Titled';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
 interface AppState {
   inputValue: string;
